abcbe569714091a6a65c7b81e7d2d5a2b812ac6d
wangtianci <wangtianci2068@gmail.com>
2023-12-25 16:14:31 +0800

    feature: 基于策略模式实现优惠卷折扣策略

---
+++ design-model-strategy/src/main/java/cn/knightzz/design/strategy/chapter03/controller/OrderController.java
@@ -0,0 +1,10 @@
+package cn.knightzz.design.strategy.chapter03.controller;
+
+/**
+ * @author 王天赐
+ * @title OrderController
+ * @description
+ * @create 2023-11-13 12:39
+ */
+public class OrderController {
+}
